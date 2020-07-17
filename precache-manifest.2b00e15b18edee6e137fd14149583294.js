4d5067e70624543525a7ad290208fb290d085d04
kgbhaumik86 <kgbhaumik86@gmail.com>
2020-07-17 17:50:28 +0530

    Updates

---
+++ precache-manifest.2b00e15b18edee6e137fd14149583294.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc292adfdb61d5140aad234f3976b0cd",
+    "url": "/kgbportfolio2/index.html"
+  },
+  {
+    "revision": "172e05b4095d750f4028",
+    "url": "/kgbportfolio2/static/css/main.3a7ff8aa.chunk.css"
+  },
+  {
+    "revision": "92277f38fd6c5d20f1b5",
+    "url": "/kgbportfolio2/static/js/2.560136a8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kgbportfolio2/static/js/2.560136a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172e05b4095d750f4028",
+    "url": "/kgbportfolio2/static/js/main.1dd36c2d.chunk.js"
+  },
+  {
+    "revision": "65b744c771da6be22dfb",
+    "url": "/kgbportfolio2/static/js/runtime-main.e7a7aaf8.js"
+  },
+  {
+    "revision": "f8648487f068e002a0ad46321d555add",
+    "url": "/kgbportfolio2/static/media/01-full.f8648487.jpg"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/kgbportfolio2/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "8fdb36b51df820ac2e7bcf6533327f6f",
+    "url": "/kgbportfolio2/static/media/02-full.8fdb36b5.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/kgbportfolio2/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "3b263f0ffc3870f7fc6cefee9a86d50b",
+    "url": "/kgbportfolio2/static/media/03-full.3b263f0f.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/kgbportfolio2/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "c58223e188b4fad806838b3355441f74",
+    "url": "/kgbportfolio2/static/media/04-full.c58223e1.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/kgbportfolio2/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "408a2be2fc16ddf874a8daac0f81c828",
+    "url": "/kgbportfolio2/static/media/05-full.408a2be2.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/kgbportfolio2/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "6379a8ad27dee3e9166a9f81b32db785",
+    "url": "/kgbportfolio2/static/media/06-full.6379a8ad.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/kgbportfolio2/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "e0b34f6fc62c82231d2016e7e85d7ed0",
+    "url": "/kgbportfolio2/static/media/1.e0b34f6f.jpg"
+  },
+  {
+    "revision": "a90b0f86f471b7840c8c3b678620cbf3",
+    "url": "/kgbportfolio2/static/media/2.a90b0f86.jpg"
+  },
+  {
+    "revision": "cc41e47b8fbdb1c81c375b65071e478c",
+    "url": "/kgbportfolio2/static/media/2.cc41e47b.jpg"
+  },
+  {
+    "revision": "2df43e3546ec0d6666badb2ddbc716ee",
+    "url": "/kgbportfolio2/static/media/3.2df43e35.jpg"
+  },
+  {
+    "revision": "e2d2a51c2932205b52f4a5ae1943eb97",
+    "url": "/kgbportfolio2/static/media/header-bg.e2d2a51c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/kgbportfolio2/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "2c06174927f9ff7f6e693f3106bd5ff7",
+    "url": "/kgbportfolio2/static/media/navbar-logo.2c061749.svg"
+  }
+]);
